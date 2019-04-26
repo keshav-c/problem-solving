d1029ef1221c0f1e4a65aec762a37e2073239020
keshav-c <keshav1985@gmail.com>
2019-04-26 16:51:43 +0530

    Re-add debugging code to leaderboard.py

--- leaderboard/leaderboard.py
+++ leaderboard/leaderboard.py
@@ -1,13 +1,21 @@
 #!/bin/python3
 
-def climbingLeaderboard(scores, alice):
+def climbingLeaderboard(scores, alice, answer):
     score_levels = [scores[0]]
     for score in scores[1:]:
         if score != score_levels[-1]:
             score_levels.append(score)
     alice_ranks = []
-    for a_score in alice:
+    for i, a_score in enumerate(alice):
+        if i < 10:
+            print(i)
         alice_ranks.append(get_rank(score_levels, a_score))
+        if i < 10:
+            print(alice_ranks[i], answer[i])
+        if (i + 1) % 500 == 0:
+            print(alice_ranks[i], answer[i])
+        if alice_ranks[i] != answer[i]:
+            raise Exception()
     return alice_ranks
 
 def get_rank(score_levels, score):
@@ -17,6 +25,7 @@ def get_rank(score_levels, score):
         return len(score_levels) + 1
     low = 0
     high = len(score_levels) - 1
+    print("max, min, score", score_levels[0], score_levels[-1], score)
     while low < high:
         mid = (low + high) // 2
         if mid == low:
@@ -44,7 +53,7 @@ if __name__ == '__main__':
     
     answer = list(map(int, open("leaderboard_output07.txt", "r").read().strip().split()))
 
-    result = climbingLeaderboard(scores, alice)
+    result = climbingLeaderboard(scores, alice, answer)
     
     print(result == answer)
 
@@ -54,5 +63,3 @@ if __name__ == '__main__':
     # print(len(scores))
     # print(alice_count)
     # print(len(alice))
-
-    # Remerge the debugging code into master    
